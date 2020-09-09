1fe3294bc56a407f0ac323be836ceb859e45f27a
Fiona Bianchi <fiona@hiklas.co.uk>
2020-09-09 22:30:30 +0100

    Fixing broken test and adding another one for WS interceptor

--- src/main/java/uk/co/hiklas/websocket/simple/websockets/stomp/StompHandshakeInterceptor.java
+++ src/main/java/uk/co/hiklas/websocket/simple/websockets/stomp/StompHandshakeInterceptor.java
@@ -1,5 +1,6 @@
 package uk.co.hiklas.websocket.simple.websockets.stomp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.stereotype.Component;
@@ -8,9 +9,10 @@ import org.springframework.web.socket.server.HandshakeInterceptor;
 
 import java.util.Map;
 
-
+@Slf4j
 @Component
 public class StompHandshakeInterceptor implements HandshakeInterceptor {
+
     @Override
     public boolean beforeHandshake(ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse, WebSocketHandler webSocketHandler, Map<String, Object> map) throws Exception {
         return false;
--- src/test/java/uk/co/hiklas/websocket/simple/websockets/stomp/StompWebSocketConfigurerTest.java
+++ src/test/java/uk/co/hiklas/websocket/simple/websockets/stomp/StompWebSocketConfigurerTest.java
@@ -9,16 +9,19 @@ import org.mockito.Mock;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.StompWebSocketEndpointRegistration;
 import org.springframework.web.socket.server.HandshakeInterceptor;
 
 import java.util.List;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.when;
 
 
 @RunWith(SpringRunner.class)
@@ -33,11 +36,17 @@ public class StompWebSocketConfigurerTest {
     @Mock
     private StompHandshakeInterceptor mockStompHandshakeInterceptor;
 
+    @Mock
+    private StompWebSocketEndpointRegistration mockStompWebSocketEndpointRegistration;
+
     @Captor
     private ArgumentCaptor<String> relayNamesArgumentCaptor;
 
     @Captor
     private ArgumentCaptor<String> endpointArgumentCaptor;
+
+    @Captor
+    private ArgumentCaptor<HandshakeInterceptor> handshakeInterceptorCaptor;
 
 
     @InjectMocks
@@ -63,13 +72,23 @@ public class StompWebSocketConfigurerTest {
 
     @Test
     public void testRegisterStompEndpoints() throws Exception {
+
+        when(mockStompEndpointRegistry.addEndpoint(any()))
+                .thenReturn(mockStompWebSocketEndpointRegistration);
+
         stompWebSocketConfigurer.registerStompEndpoints(mockStompEndpointRegistry);
 
         verify(mockStompEndpointRegistry, times(1))
                 .addEndpoint(endpointArgumentCaptor.capture());
 
+        verify(mockStompWebSocketEndpointRegistration, times(1))
+                .addInterceptors(handshakeInterceptorCaptor.capture());
+
         String endpoint = endpointArgumentCaptor.getValue();
         assertThat(endpoint, equalTo("/ankh"));
+
+        HandshakeInterceptor handshakeInterceptor = handshakeInterceptorCaptor.getValue();
+        assertThat(handshakeInterceptor, equalTo(mockStompHandshakeInterceptor));
     }
 
 }
